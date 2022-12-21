25972f79bf243c9b2f9a3b9fd77fb1f92473b945
MaddalenaMonsignori <mm010044@infolab01>
2022-12-21 16:42:11 +0100

    es

--- es7dic_reco.py
+++ es7dic_reco.py
@@ -6,10 +6,14 @@ Time Stamp rivelazione
 '''
 class Hit():
     
-    def __init__(self, id_modulo, id_sensore, tempo_rivelazione):
+    def __init__(self, id_modulo, id_sensore, time):
         self.id_modulo = id_modulo
         self.id_sensore = id_sensore
-        self.tempo_rivelazione = tempo_rivelazione
+        self.time= time
+    def __lt__(self, other):
+        return self.time < other.time
+    def __sub__(self, other):
+        return self.time - other.time
 
 '''Definisco la classe Event in cui un oggetto di tipo Event deve contiene informazioni su:
 Numero di Hit
@@ -21,21 +25,20 @@ Array di tutti gli Hit
 
 class Event():
 
-    def __init__(self, num_hit, time_primo_hit, time_ultimo_hit, durata_tempo, array_hit):
-        self.num_hit = num_hit
-        self.time_primo_hit = time_primo_hit
-        self.time_ultimo_hit = time_ultimo_hit
-        self.durata_tempo=durata_tempo
+    def __init__(self):
+        self.num_hit = 0
+        self.time_primo_hit = 0
+        self.time_ultimo_hit = -1
+        self.durata_tempo= -1
         self.array_hit=np.empty([])
 
 #e' necessario inserire una funzione aggiorna event perche' nel costruttore init non so come passare come parametro un array e in piu' num_hit, time_primo_hit, time_ultimo_hit, durata_tempo dipendono dal contenuto dell'array quindi devo calcolarli
     def aggiorna_event(array):
-        for i in array:
-            np.append(self.array_hit,i)
-        self.num_hit=len(array_hit)
-        self.time_primo_hit=self.array_hit[0]
-        self.time_ultimo_hit=self.array_hit[len(array_hit)]
-        self.durata_tempo=self.time_ultimo_hit-self.time_primo_hit
+        self.num_hit=self.num_hit+1
+        self.array_hit=np.append(self.arrray_hit, h)
+        self.time_primo_hit=self.array_hit[0].time
+        self.time_ultimo_hit=h.time
+        self.durata_tempo=h.time-self.array_hit[0]
 
 #stampa le info su event
     def stampa_event():
